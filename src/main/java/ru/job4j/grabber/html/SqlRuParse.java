a47ae33d87f2f1adae3224c160f1b96c77be842f
Maria <ronios@yandex.ru>
2022-01-25 14:17:49 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -4,8 +4,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.time.LocalDateTime;
 
 public class SqlRuParse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
     public static void main(String[] args) throws Exception {
         for (int i = 1; i < 6; i++) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
@@ -18,5 +32,24 @@ public class SqlRuParse {
                 System.out.println(href.text());
             }
         }
+    }
+
+    public Post getPostFromLink(String link) throws IOException, ParseException {
+        Document doc = Jsoup.connect(link).get();
+        Post post = new Post();
+        post.setDescription(getDescriptionFromDoc(doc));
+        post.setCreated(getCreatedFromDoc(doc));
+        return post;
+    }
+
+    private String getDescriptionFromDoc(Document doc) {
+        Elements bodyElements = doc.select(".msgBody");
+        return bodyElements.get(1).text();
+    }
+
+    private LocalDateTime getCreatedFromDoc(Document doc) throws ParseException {
+        Elements footerElements = doc.select(".msgFooter");
+        String[] footerStrings = footerElements.get(0).text().split("\\[");
+        return dateTimeParser.parse(footerStrings[0].trim());
     }
 }
